2f126a7d30cedc672f236133144eed36c374171c
Majharul tanvir <mdtanvir6070@gmail.com>
2022-05-27 17:01:53 +0600

    manage all products

--- src/pages/Dashboard/ManageAllProducts.js
+++ src/pages/Dashboard/ManageAllProducts.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../../Shared/Loading'
 
 const ManageAllProducts = () => {
+    const { data: tools, isLoading } = useQuery('tools', () => fetch('http://localhost:5000/tools').then(res => res.json()))
+
+    if (isLoading) {
+        return <Loading />
+    }
     return (
-        <div>
-            
+        <div className="overflow-x-auto mx-10 my-10">
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th>Sl no.</th>
+                        <th>Product name</th>
+                        <th>image</th>
+                        <th>Available</th>
+                        <th>Minimum shipping</th>
+                        <th>price</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody className='text-center'>
+                    {
+                        tools?.map((tool, i) => <tr key={tool._id}>
+                            <th>{i + 1}</th>
+                            <td>{tool.name}</td>
+                            <td><img className='w-12' src={tool.image} alt="" /></td>
+                            <td>{tool.available}</td>
+                            <td>{tool.minQuantity}</td>
+                            <td>{tool.price}</td>
+                            <td>Blue</td>
+                        </tr>)
+                    }
+
+                </tbody>
+            </table>
         </div>
     );
 };
