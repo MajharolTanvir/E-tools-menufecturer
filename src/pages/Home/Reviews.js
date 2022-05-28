66735144a9c5cb44816f2517c23319a2c92c4c44
Majharul tanvir <mdtanvir6070@gmail.com>
2022-05-28 19:31:31 +0600

    resolving

--- src/pages/Home/Reviews.js
+++ src/pages/Home/Reviews.js
@@ -8,7 +8,6 @@ const Reviews = () => {
     if (isLoading) {
         return <Loading />
     }
-    console.log(reviews);
 
     return (
         <div className="container mx-auto">
@@ -37,7 +36,7 @@ const Reviews = () => {
                                                     <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
                                                     <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
                                                     <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                                                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" defaultChecked />
+                                                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
                                                     <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
                                                 </div>
                                             }
@@ -46,7 +45,7 @@ const Reviews = () => {
                                                     <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
                                                     <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
                                                     <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                                                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" defaultChecked />
+                                                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
                                                 </div>
                                             }
                                             {
